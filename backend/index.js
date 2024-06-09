9641d3b2faec13b3954fd50df683556a02a854cc
Alan Tello Oyola <alan.tello@gmail.com>
2024-06-09 11:39:03 -0500

    add endpoint for product detail

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,7 @@ const port = 3100
 app.use(express.json())
 app.use(cors())
 
-
-
+// endpoint products
 app.get('/api/products', (req, res) => {
     // Create the connection to database
     const connection = mysql.createConnection({
@@ -29,6 +28,29 @@ app.get('/api/products', (req, res) => {
             res.send(results)
         }
     );
+})
+
+// endpoint product detail
+app.get('/api/products/:id', (req, res) => {
+    // Create the connection to database
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'alan',
+        password: '1234',
+        database: 'mplace_app',
+    });
+
+    // product id
+    const productId = req.params.id;
+
+    // simple query to get a product
+    const query = 'SELECT * FROM `products` WHERE id = ?';
+    connection.query(query, [productId], function (err, results, fields) {
+        //console.log(results); // results contains rows returned by server
+        //console.log(fields); // fields contains extra meta data about results, if available
+
+        res.send(results)
+    });
 })
 
 app.listen(port, () => {
